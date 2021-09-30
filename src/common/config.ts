1f6d32da6d55a78f5875c32e9c6025d21fd194a7
jinmeng.mu <jinmeng.mu@sfere-elec.com>
2021-09-30 18:09:08 +0800

    完成道具第一部分

--- src/common/config.ts
+++ src/common/config.ts
@@ -3,6 +3,6 @@ export default {
   col: 12,
   interval: 1000,
   //serverUrl: "http://localhost:4567/",
-  serverUrl: "http://192.168.4.24:4567/",
-  //serverUrl: "http://103.216.154.42:4567/",
+  //serverUrl: "http://192.168.4.24:4567/",
+  serverUrl: "http://103.216.154.42:4567/",
 };
